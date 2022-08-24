a600f2e08440334adcf24a9fa84efa15f652d346
Thiel Serra <thiel.serra@gmail.com>
2022-08-24 17:52:02 -0300

    Adicionando arquivos para configuração de ambiente com docker

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> B-Docker | PHP 5.6 </title>
+    <style>
+        .text-center {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="text-center">
+        <h1> B-Docker | PHP 5.6 </h1>
+        <span > container em execução...</span>
+
+        <hr>
+
+        <?php echo phpinfo(); ?>
+    </div>
+</body>
+</html>
